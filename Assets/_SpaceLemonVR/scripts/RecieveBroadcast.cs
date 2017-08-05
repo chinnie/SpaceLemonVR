21d9379a6a42ad284cdd25fba95f9acfc65e8b92
chinnie <chinniechinchilla@yahoo.com>
2017-08-05 14:54:08 -0700

    Jami's scene & scripts, new chair model

---
+++ Assets/_SpaceLemonVR/scripts/RecieveBroadcast.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecieveBroadcast : MonoBehaviour {
+
+    [SerializeField] private AudioSource backgroundStatic;
+    [SerializeField] private AudioSource broadcastMessage;
+    [SerializeField] private Vector3 sourceBroadcast;
+    [SerializeField] private float sensativity = 1.0f; 
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        backgroundStatic.volume = 1;
+        broadcastMessage.volume = 0;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    Vector3 facing = transform.forward;
+	    float amplitude =  Vector3.Dot(facing, sourceBroadcast); //if facing and sourceBroadcase in same direction will return 1
+
+	    if (amplitude > 0)
+	    {
+	        broadcastMessage.volume = amplitude;
+	        backgroundStatic.volume = 1 - amplitude;
+	    }
+	    else
+	    {
+            backgroundStatic.volume = 1;
+            broadcastMessage.volume = 0;
+        }
+	}
+}
