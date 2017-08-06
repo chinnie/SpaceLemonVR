84577bfd611de1114ecec3ee566a61c538c82a08
Livio De La Cruz <livio@livio.me>
2017-08-05 23:00:18 -0700

    added some hilarious manual pages

---
+++ Assets/_SpaceLemonVR/Manual/TableOfContents.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TableOfContents : MonoBehaviour
+{
+	public Transform canvas;
+	public TMPro.TextMeshProUGUI text;
+
+	private void Awake()
+	{
+		string table = "";
+		for(int i = 0; i < canvas.childCount; i++)
+		{
+			table += i + ". " + canvas.GetChild(i).name + "\n";
+		}
+		text.text = table;
+	}
+}
