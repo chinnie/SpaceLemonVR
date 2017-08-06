4a360c42732c9366a65e2c5f1e992c3fd70ae21d
Livio De La Cruz <livio@livio.me>
2017-08-06 16:00:51 -0700

    cabinet covers/shows wires better

---
+++ Assets/_SpaceLemonVR/scripts/TopCabinet.cs
@@ -0,0 +1,19 @@
+﻿using NewtonVR;
+using UnityEngine;
+
+public class TopCabinet : NewtonVR.NVRInteractableItem
+{
+	public GameObject wires;
+	private bool firstTime = true;
+
+	public override void BeginInteraction(NVRHand hand)
+	{
+		base.BeginInteraction(hand);
+		if (firstTime)
+		{
+			ForceDetach(hand);
+			wires.SetActive(true);
+		}
+		firstTime = false;
+	}
+}
