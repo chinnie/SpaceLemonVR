6f9a095ca561b5937c751b5bb52c2a22b24faebf
Clinton <clinton.reddie@live.com>
2017-08-06 12:29:08 -0700

    happy plugs and better lights for wire puzzle

--- Assets/_SpaceLemonVR/scripts/SwitchboardLight.cs
+++ Assets/_SpaceLemonVR/scripts/SwitchboardLight.cs
@@ -8,18 +8,19 @@ public class SwitchboardLight : MonoBehaviour
 
 	public int timesActive;
 
-	[SerializeField] private Material offMat;
-	[SerializeField] private Material onMat;
+	[SerializeField] private Color offCol;
+	[SerializeField] private Color onCol;
 
 	void Start()
 	{
 		thisRend = GetComponent<Renderer> ();
+		thisRend.material.SetColor ("_EmissionColor", offCol);
 	}
 
 	public void TurnOn()
 	{
-		if (timesActive > 0 && onMat != thisRend.material)
-			thisRend.material = onMat;
+		if (timesActive > 0 && onCol != thisRend.material.GetColor ("_EmissionColor"))
+			thisRend.material.SetColor ("_EmissionColor", onCol);
 	}
 
 	public void Activate(bool active)
@@ -27,14 +28,14 @@ public class SwitchboardLight : MonoBehaviour
 		if (active) 
 		{
 			timesActive += 1;
-			if (timesActive > 0 && onMat != thisRend.material)
-				thisRend.material = onMat;
+			if (timesActive > 0 && onCol != thisRend.material.GetColor ("_EmissionColor"))
+				thisRend.material.SetColor ("_EmissionColor", onCol);
 		} 
 		else 
 		{
 			timesActive -= 1;
-			if (timesActive < 1 && offMat != thisRend.material)
-				thisRend.material = offMat;
+			if(timesActive < 1 && offCol != thisRend.material.GetColor ("_EmissionColor"))
+				thisRend.material.SetColor ("_EmissionColor", offCol);
 		}
 	}
 }
